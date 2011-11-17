676a955afc98f859f40206098525f523895d3152
Yanick Witschi <yanick.witschi@certo-net.ch>
2011-11-17 17:19:00 +0100

    Initial commit

---
+++ config/config.php
@@ -0,0 +1,37 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2010 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  certo web & design GmbH 2010 - 2011 
+ * @author     Yanick Witschi <yanick.witschi@certo-net.ch> 
+ * @package    easyExclude 
+ * @license    LGPL 
+ * @filesource
+ */
+
+/**
+ * Hooks
+ */
+$GLOBALS['TL_HOOKS']['outputBackendTemplate'][] = array('EasyExclude', 'addUsergroupSelect');
+$GLOBALS['TL_HOOKS']['loadDataContainer'][] = array('EasyExclude', 'addEasyExclude');
+$GLOBALS['TL_HOOKS']['executePostActions'][] = array('EasyExclude', 'doAjaxForMe');
+?>
---
+++ html/easyExclude_src.js
@@ -0,0 +1,271 @@
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2010 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  certo web & design GmbH 2010 - 2011
+ * @author     Yanick Witschi <http://www.certo-net.ch>
+ * @package    Backend
+ * @license    LGPL
+ * @filesource
+ */
+
+var needsRequestToken = (window.Request.Contao) ? true : false;
+
+/**
+ * Class Request.Compatibility
+ * Does the appropriate thing according to whether it's a 2.10 Contao not (REQUEST TOKENS)
+ */
+Request.Compatibility = new Class({
+	Extends: (needsRequestToken) ? Request.Contao : Request.Mixed,
+	initialize: function(options)
+	{
+			this.parent(options);
+	},
+	send: function(data)
+	{
+			if (needsRequestToken)
+				this.options.data += '&REQUEST_TOKEN=' + REQUEST_TOKEN;
+
+			this.parent(data); 
+	}
+});
+
+
+/**
+ * Class EasyExclude
+ */
+var EasyExclude = new Class({
+	Implements: [Options],
+	options:
+	{
+		table: ''
+	},
+	
+	//Properties
+	fields: null,
+	usergroup: 0,
+	checkboxes: {},
+	
+	/**
+	 * Initialize
+	 */
+	initialize: function(options)
+	{
+	    var self = this;
+		this.setOptions(options);
+		
+		// add the onchange event to the usergroup dropdown
+		$('easyExclude_usergroup').addEvent('change', function()
+		{
+		     // get all fields
+            self.fields = $$('div.easyExclude');
+            
+		    self.usergroup = this.get('value');
+		    if(self.usergroup > 0)
+		    {
+		        self.cleanTheScreen();
+		        self.addCheckboxes();
+                self.updateStates(); 
+		    }
+		    else
+		    {
+                self.cleanTheScreen();                
+		    }
+		});
+		
+		// how great we have a hook to register functions upon the event that occurs whenever a subpalette is being loaded!!
+        window.addEvent('subpalette', function()
+        {
+            $('easyExclude_usergroup').fireEvent('change');
+        });
+	},
+
+	/**
+	 * Get all fields
+	 */
+	updateStates: function() 
+	{
+	    var self = this;
+
+        new Request.Compatibility(
+        {
+            url: window.location.href,
+            data: 'isAjax=1&action=easyExcludeGetFieldRights&usergroup=' + this.usergroup + '&table=' + this.options.table,
+            onRequest: function()
+            {
+                AjaxRequest.displayBox('Loading data …');
+            },
+            onComplete: function()
+            {
+                AjaxRequest.hideBox();
+            },
+            onFailure: function()
+            {
+                alert('failed');
+            },
+            onSuccess: function(response, responseText)
+            {	
+                if(response)
+                {
+                	// unfortunately Request.Mixed doesn't decode JSON automatically
+					var decoded = JSON.decode(response);
+					response = (decoded) ? decoded : response;
+
+                    response.each(function(allowedField)
+                    {
+                        // set checkboxes true
+                        var key = 'easyExclude_' + allowedField + '_' + self.usergroup;
+
+                        if($(key))
+                        {
+                            $(key).checked = true;
+                        }
+                    });
+                }
+                
+                // update the background color
+                self.changeBackgroundColor();
+            }
+        }).send();      
+	},
+	
+	
+    /**
+     * Inject checkboxes 
+     */
+    addCheckboxes: function()
+    {
+        var self = this;
+
+        this.fields.each(function(field)
+        {
+            var key = 'easyExclude_' + self.filterForFieldName(field.get('class')) + '_' + self.usergroup;
+            var cbx = null;
+            
+            // load the disposed element if there is already one, otherwise generate it the first time
+            if(self.checkboxes[key])
+            {
+                cbx = self.checkboxes[key];
+            }
+            else
+            {
+                cbx = new Element('input', {
+                    type: 'checkbox',
+                    name: key,
+                    id: key,
+                    'class': 'tl_checkbox easyExcludeCheckbox',
+                    events:
+                    {
+                        change: function()
+                        {
+                            self.saveChange(field, this);
+                        }
+                    }
+                });              
+            }
+
+            // and inject it into the field 
+            cbx.inject(field, 'top');
+        });
+    },
+	 
+	 
+    saveChange: function(field, cbx)
+    {
+        var self = this;
+        var state = (cbx.checked) ? 1 : 0;
+        
+        var objRequest = new Request.Compatibility(
+        {
+            url: window.location.href,
+            data: 'isAjax=1&action=easyExcludeSaveChange&usergroup=' + this.usergroup + '&field=' + self.filterForFieldName(field.get('class')) + '&table=' + this.options.table + '&state=' + state,
+            onRequest: function()
+            {
+                AjaxRequest.displayBox('Loading data …');
+            },
+            onComplete: function()
+            {
+                AjaxRequest.hideBox();
+            },
+            onFailure: function()
+            {
+                alert('failed');
+            },
+            onSuccess: function(responseJSON, responseText)
+            {
+                // update the background color
+                self.changeBackgroundColor();
+            }
+        }).send();
+	 },
+	 
+	 
+	 /**
+	  * Filters a class string for the field name
+	  * @param string
+	  * @return string
+	  */
+	  filterForFieldName: function(classString)
+	  {
+	      var matches = /easyExcludeFN_([\S]+)/i.exec(classString);
+	      return matches[1];
+	  },
+	  
+	  
+	  /**
+	   * Change bg color according to the checkbox state
+	   */
+	   changeBackgroundColor: function()
+	   {
+	       $$('input.easyExcludeCheckbox').each(function(cbx)
+	       {
+                var field = cbx.getParent('div.easyExclude');
+                if(cbx.checked)
+                {
+                    field.setStyle('background', '#E4FFD4');
+                }
+                else
+                {
+                    field.setStyle('background', '#EBEBEB');
+                }                
+	       });
+	   },
+	   
+	   
+	   /**
+	    * Clean the screen
+	    */
+	    cleanTheScreen: function()
+	    {
+            // dispose checkboxes
+            $$('input.easyExcludeCheckbox').each(function(cbx)
+            {
+                var key = cbx.get('id');                
+                // add it to the object literal
+                this.checkboxes[key] = cbx.dispose();
+            }.bind(this));
+
+            // update the background color
+            this.fields.each(function(field)
+            {
+                field.setStyle('background', '');
+            });   
+	    }
+});
